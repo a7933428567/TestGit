fd2e4448a08836aab2946356721c7ed95e540d84
liuxiting <liuxiting@liuxitingdeMac-mini.local>
2015-04-13 10:11:17 +0800

    4.13.2

---
+++ myGit/myGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myGit
+//
+//  Created by liuxiting on 15/4/13.
+//  Copyright (c) 2015年 liuxiting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myGit/myGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myGit
+//
+//  Created by liuxiting on 15/4/13.
+//  Copyright (c) 2015年 liuxiting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
